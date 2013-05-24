888a584fb609ce7f556c882b64fe0b40efcaa148
Brooss <brooss.teambb@gmail.com>
2013-05-25 09:09:38 +1000

    add libopus

--- jni/Android.mk
+++ jni/Android.mk
@@ -31,18 +31,24 @@ LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libvorbisenc.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/vorbis/
 LOCAL_STATIC_LIBRARIES := libogg-prebuilt libvorbis-prebuilt
 include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS) 
+LOCAL_MODULE := libopus-prebuilt 
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libopus.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/opus/
+LOCAL_STATIC_LIBRARIES := 
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS) 
 LOCAL_MODULE := libkrad-prebuilt 
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libkrad.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/krad_nanolib
-LOCAL_STATIC_LIBRARIES := libogg-prebuilt libvorbis-prebuilt libvorbisenc-prebuilt libvpxenc-prebuilt
+LOCAL_STATIC_LIBRARIES := libogg-prebuilt libvorbis-prebuilt libvorbisenc-prebuilt libvpxenc-prebuilt libopus-prebuilt
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := krcam
 LOCAL_SRC_FILES := krcam.cpp krcam_util.c
-LOCAL_STATIC_LIBRARIES := cpufeatures libogg-prebuilt libvpx-prebuilt libvorbisenc-prebuilt libvorbis-prebuilt libkrad-prebuilt 
+LOCAL_STATIC_LIBRARIES := cpufeatures libogg-prebuilt libvpx-prebuilt libvorbisenc-prebuilt libvorbis-prebuilt  libopus-prebuilt libkrad-prebuilt 
 LOCAL_CFLAGS 	:= -D__STDC_CONSTANT_MACROS
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
 LOCAL_ARM_MODE := arm
